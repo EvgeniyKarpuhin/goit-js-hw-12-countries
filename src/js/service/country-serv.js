19427622e8468fa66ae7d7fc19a14dcd155ea7a1
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2021-09-22 01:11:06 +0300

    Update country-serv.js

--- src/js/service/country-serv.js
+++ src/js/service/country-serv.js
@@ -3,6 +3,6 @@ const base = 'https://restcountries.eu/rest/v2/name/';
 export default {
     fetchArticles(query) {
         const param = `${query}`;
-        return Object.fetch(base + param).then(e => e.json());
+        return Object.fetchArticles(base + param).then(e => e.json());
     },
 };
