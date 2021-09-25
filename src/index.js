9b00044ee5f2e8fd0fe408da7207bfdd36dc66e0
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2021-09-25 16:50:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -32,10 +32,11 @@ function countryInput(e) {
 
 
 function buildList(count, template) {
+    clearArticles()
     const mark = count.map(c => template(c)).join();
     refs.search.insertAdjacentHTML('afterbegin', mark)
 }
 
-// function clearArticles() {
-//     refs.search.innerHTML = '';
-// }
+function clearArticles() {
+    refs.search.innerHTML = '';
+}
